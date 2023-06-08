658c7fac0ae4598944098fc14f0067209d748f52
BraianPerez97 <3603@holbertonstudents.com>
2023-06-08 17:36:12 -0400

    fixed indented spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     for i in matrix[0:]:
+    for row in matrix:
         spc = ""
-        for j in i[0:]:
-            print("{:s}{:d}".format(spc, j), end="")
+        for element in row:
+            print("{:s}{:d}".format(spc, element), end="")
             spc = " "
         print()
