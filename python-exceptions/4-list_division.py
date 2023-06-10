f526eb4b3870aa7c9a49125aa0aa8e876e49aa03
BraianPerez97 <3603@holbertonstudents.com>
2023-06-09 21:29:09 -0400

    fixed indentation for try and except

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def list_divisioni(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            result = my_list_1[1] / my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print ("division by 0")
+            print("division by 0")
             result = 0
         except TypeError:
             print("wrong type")
