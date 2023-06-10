342e6da323e3aad9679c7ded9d93c9305fc1e4a6
BraianPerez97 <3603@holbertonstudents.com>
2023-06-09 21:27:32 -0400

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,7 +9,11 @@ def list_divisioni(my_list_1, my_list_2, list_length):
             result = 0
         except TypeError:
             print("wrong type")
+            result = 0
         except IndexError:
+            print("out of range")
+            result = 0
+        finally:
             new_list.append(result)
 
     return (new_list)
