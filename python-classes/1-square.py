99925de46e7e1cd3ae584baeb9aeb38972e8c7a4
BraianPerez97 <3603@holbertonstudents.com>
2023-06-22 11:24:44 -0400

    should work

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """Python OOP: Classes
 """
+
 
 class Square:
 
@@ -9,6 +9,7 @@ class Square:
     """
 
     def __init__(self, size):
+
         """Initialize method that stores the size of the square
 
         Args:
