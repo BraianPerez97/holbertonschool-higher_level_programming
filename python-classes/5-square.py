1fffecef89af65dc3d306eb294c2fba42f5177fe
BraianPerez97 <3603@holbertonstudents.com>
2023-06-22 11:39:17 -0400

    fixing comment indentation

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+""""Python OOP: classes
+"""
+
+
 class Square:
+
     """ A class that defines a square by its size
     """
+
     def __init__(self, size=0):
+
         """ Method to initialize the square object
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -13,20 +21,26 @@ class Square:
             self.__size = size
 
     def area(self):
+
         """ Method that returns the square are of the object
         """
+
         return (self.__size ** 2)
 
     @property
     def size(self):
+
         """ Method to returns the size value
         """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+
         """ Method to set the size value of the square object
         """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -35,9 +49,11 @@ class Square:
             self.__size = value
 
     def my_print(self):
+
         """ Method that prints a # square according
         to the size value
         """
+
         if not self.__size:
             print()
         else:
