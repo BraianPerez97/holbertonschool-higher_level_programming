d370432d1abaf35b33e31035a20c3334b1a3ac18
BraianPerez97 <3603@holbertonstudents.com>
2023-06-26 13:23:55 -0400

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""python OOP: ingeritance
+"""
+
+def lookup(obj):
+
+    """function that returns the list of available attributes
+    and methods of an object
+
+    args:
+        obj: instance of the class
+
+    Returns:
+        List of attributes
+    """
+    return dir(obj)
