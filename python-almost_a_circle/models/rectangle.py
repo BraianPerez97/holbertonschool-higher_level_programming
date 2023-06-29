62bd27d87f0b08c0233508bcb06cb5ab0b777183
BraianPerez97 <3603@holbertonstudents.com>
2023-06-29 18:13:04 -0400

    added str self

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,6 @@ class Rectangle(Base):
         """Display Getter"""
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
