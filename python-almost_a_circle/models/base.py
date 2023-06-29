f455620c371380efa65b71d90ce7d4fefb6a96e3
BraianPerez97 <3603@holbertonstudents.com>
2023-06-29 19:54:15 -0400

    class  methods

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Class base"""
 import json
-
+import os.path
+import csv
 
 class Base:
     """Class Base"""
@@ -14,3 +15,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """List to JSON string"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save objects in a file"""
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+        if not list_objs:
+            pass
+        else:
+            for i in range(len
