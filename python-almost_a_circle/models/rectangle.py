116f1d589005e1930e0eda30aa06a14244ed0c17
BraianPerez97 <3603@holbertonstudents.com>
2023-06-29 17:53:09 -0400

    fixed typos

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """Module that contains class Rectangle,
 inheritance of class Base
 """
+import json
 from models.base import Base
 
 class Rectangle(Base):
@@ -25,7 +26,7 @@ class Rectangle(Base):
         """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -39,7 +40,7 @@ class Rectangle(Base):
         """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
