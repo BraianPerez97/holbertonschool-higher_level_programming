2647e06250e7254bc538d5fef5eda730900e16a4
BraianPerez97 <3603@holbertonstudents.com>
2023-06-29 20:12:03 -0400

    test base

--- python-almost_a_circle/tests/test_modules/test_base.py
+++ python-almost_a_circle/tests/test_modules/test_base.py
@@ -1,77 +1,97 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+""" Module for test Base class """
 import unittest
-import calc
-
-"""Module Comment
-
-    Test casses: a test class that inherits the 'unittest.Testcase'
-"""
-
-
-class TestCalc(unittest.TestCase):
-
-    """"must start with 'test_' (otherwise: "add_test") it gets skipped
-
-    Example: testing the add Functions
-    """
-
-    def test_add(self):
-
-        """built-in functions documantation are online ('sefl.assetExample')
-            Edge Casses:
-                Errors could happen within this function
-        """
-
-        """Edge Case: two positves"""
-        self.assertEqual(calc.add(10, 5), 15)
-
-        """Edge Case: one positive & one negative"""
-        self.assertEqual(calc.add(-1, 1), 0)
-
-        """Edge Case: two negatives"""
-        self.assertEqual(calc.add(-1, -1) -2)
+import os
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
 
 
-    def test_subtract(self):
-        """Edge Case: two positves"""
-        self.assertEqual(calc.subtract(10, 5), 5)
+class TestBaseMethods(unittest.TestCase):
+    """ Suite to test Base class """
 
-        """Edge Case: one positive & one negative"""
-        self.assertEqual(calc.subtract(-1, 1), -2)
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
 
-        """Edge Case: two negatives"""
-        self.assertEqual(calc.subtract(-1, -1), 0)
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
 
+    def test_id_default(self):
+        """ Test default id """
+        new = Base()
+        self.assertEqual(new.id, 1)
 
-    def test_multiply(self):
-            """Edge Case: two positves"""
-            self.assertEqual(calc.multiply(10, 5), 50)
+    def test_id_nb_objects(self):
+        """ Test nb object attribute """
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
 
-            """Edge Case: one positive & one negative"""
-            self.assertEqual(calc.multiply(-1, 1), -1)
+    def test_id_mix(self):
+        """ Test nb object attributes and assigned id """
+        new = Base()
+        new2 = Base(1024)
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 1024)
+        self.assertEqual(new3.id, 2)
 
-            """Edge Case: two negatives"""
-            self.assertEqual(calc.multiply(-1, -1), 1)
+    def test_string_id(self):
+        """ Test string id """
+        new = Base('1')
+        self.assertEqual(new.id, '1')
 
+    def test_more_args_id(self):
+        """ Test passing more args to init method """
+        with self.assertRaises(TypeError):
+            new = Base(1, 1)
 
-    def test_divide(self):
+    def test_access_private_attrs(self):
+        """ Test accessing to private attributes """
+        new = Base()
+        with self.assertRaises(AttributeError):
+            new.__nb_objects
 
-            """Edge Case: two positves"""
-            self.assertEqual(calc.divide(10, 5), -1)
+    def test_save_to_file_1(self):
+        """ Test JSON file """
+        Square.save_to_file(None)
+        res = "[]\n"
+        with open("Square.json", "r") as file:
+            with patch('sys.stdout', new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
 
-            """Edge Case: one positive & one negative"""
-            self.assertEqual(calc.divide(-1, 1), -1)
+        try:
+            os.remove("Square.json")
+        except:
+            pass
 
-            """Edge Case: two negatives"""
-            self.assertEqual(calc.divide(-1, -1), 1)
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
 
-            """Edge Case: floor division"""
-            self.assertEqual(calc.divide(5, 2), 2.5)
+    def test_save_to_file_2(self):
+        """ Test JSON file """
+        Rectangle.save_to_file(None)
+        res = "[]\n"
+        with open("Rectangle.json", "r") as file:
+            with patch('sys.stdout', new=StringIO()) as str_out:
+                print(file.read())
+                self.assertEqual(str_out.getvalue(), res)
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
 
-            """Testing Exception:
-
-                Context Manager
-            """
-
-            with self.assertRaise(ValueError):
-                calc.divide(10, 0)
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
