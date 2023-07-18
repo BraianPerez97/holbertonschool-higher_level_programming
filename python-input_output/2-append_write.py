2d59bc096916a30b73543e69111c6b68b355721c
BraianPerez97 <3603@holbertonstudents.com>
2023-07-18 13:48:44 -0400

    appened write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Python: Input and Output"""
+
+
+def append_write(filename="", text=""):
+    """Function to append a string to the end of a text file and return the number of charaters appeneded"""
+    with open(filename, "a", encoding="utf-8") as f:
+        num_char = f.write(text)
+    return num_char
