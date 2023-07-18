b98759727de976a349600d364cfb82370481689a
BraianPerez97 <3603@holbertonstudents.com>
2023-07-18 13:05:44 -0400

    Read File

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Python: Input and Output"""
+
+def read_file(filename=""):
+    """Open file for reading"""
+    with open(filename, "r", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
