d8a3e5c8754a27c55cef26176519928f76ad7bbd
BraianPerez97 <3603@holbertonstudents.com>
2023-07-18 13:43:05 -0400

    write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!?usr/bin/python3
+"""Python: Input and Output"""
+
+
+def write_file(filename="", text="utf-8"):
+    """Write string to a text file. and returns the num of characters"""
+    with open(filename, "w", encoding="utf-8") as f:
+        num_char = f.write(text)
+    return num_char
