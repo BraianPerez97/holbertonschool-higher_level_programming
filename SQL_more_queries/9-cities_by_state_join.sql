6e58518219e2a1ff80d2cdd67348ba607bfaa174
BraianPerez97 <3603@holbertonstudents.com>
2023-07-26 19:54:47 -0400

    fixed trailing spaces

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script that lists all cities contained in the database
 SELECT cities.id, cities.name, states.name -- Query to join cities and states
 FROM cities
-INNER JOIN states ON cities.state_id = states.state_id
+INNER JOIN states ON cities.state_id=states.state_id
 ORDER BY cities.id
