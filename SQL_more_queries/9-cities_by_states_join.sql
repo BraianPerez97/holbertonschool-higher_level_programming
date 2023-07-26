a230de568a3cfa844f24c9795ace83210a06cb5e
BraianPerez97 <3603@holbertonstudents.com>
2023-07-26 19:45:59 -0400

    fixied typos

--- SQL_more_queries/9-cities_by_states_join.sql
+++ SQL_more_queries/9-cities_by_states_join.sql
@@ -1,4 +1,5 @@
 -- Script that lists all cities contained in the database
 SELECT cities.id, cities.name, states.name -- Query to join cities and states
 FROM cities
-JOIN states ON cities.state_id = states.id;
+INNER JOIN states ON cities.state_id = states.state_id
+ORDER BY cities.id
