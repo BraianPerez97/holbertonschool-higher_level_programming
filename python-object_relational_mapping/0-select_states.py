a48397403e96c3ade0ce7fcc5f6220b36a905b17
BraianPerez97 <3603@holbertonstudents.com>
2023-07-30 15:00:51 -0400

    initial commit and select states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Relational mapping
+"""
+
+import sys
+import MySQLdb
+
+"""Connect to MySQL database"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host= 'localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=2206
+    )
+    """run query to retrieve all rows
+    form the table"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+        cur.close()
+        db.close()
