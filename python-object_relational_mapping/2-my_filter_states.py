91b548d81c43fe48ad6765cc08f8cb2eff00e1ac
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 17:12:48 -0400

    addding filters to states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
+        state_name_searched=sys.argv[4],
         port=3306)
+
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE NAME LIKE '{}' ORDER BY states.id;".format)
+        "SELECT * FROM states WHERE NAME LIKE '{}' ORDER BY states.id;".format(state_name_searched))
     states = cur.fetchall()
 
     for state in states:
