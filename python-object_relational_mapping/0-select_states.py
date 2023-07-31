e19eb4c1b2ba1134ae1995a8fba6c28bbfc6ab25
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 14:10:43 -0400

    fixing comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,14 @@
 """Display all states from database
 """
 
+
 import sys
 import MySQLdb
 
 """Connect to MySQL database"""
 if __name__ == '__main__':
     db = MySQLdb.connect(
-        host= 'localhost',
+        host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
