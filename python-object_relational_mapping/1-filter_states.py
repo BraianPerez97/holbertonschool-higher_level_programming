9f9e4bf92ef83884c242cff4c8eb6a2df289dec9
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 15:50:58 -0400

    trying to get the code running

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,18 +9,14 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
 
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE NAME LIKE 'N%';")
+    cur.execute("SELECT * FROM states;")
     states = cur.fetchall()
 
     for state in states:
-        print(states)
+        print(state)
