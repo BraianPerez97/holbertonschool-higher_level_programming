cde27499061de42c414f442277084ce7f267e411
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 15:52:30 -0400

    still

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,25 +2,20 @@
 """
 Display all states from database
 """
+
+
 import sys
 import MySQLdb
 
-
-"""Connect to MySQL database"""
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
     states = cur.fetchall()
 
     for state in states:
         print(state)
-
-    cur.close()
-    db.close()
