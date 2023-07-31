cd579b44383e78b5f0ea2c4053b91aeec8fb46e4
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 17:10:37 -0400

    addding filters to states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,16 +5,10 @@
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    state_name_searched = sys.argv[4]
-    port = 3306
-
     db = MySQLdb.connect(
-        host="localhost",
-        user=user,
-        db=db,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306)
     cur = db.cursor()
     cur.execute(
