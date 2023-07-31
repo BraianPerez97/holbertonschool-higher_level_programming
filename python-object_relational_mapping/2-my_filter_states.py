f0b37bdb433d493aa962a8421121d0f4ef58eb26
BraianPerez97 <3603@holbertonstudents.com>
2023-07-31 17:19:11 -0400

    broke line

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,26 +2,27 @@
 """lists states
 """
 
+
 import MySQLdb
 import sys
-if __name__ == "__main__":
-    user = sys.argv[1],
-    passwd = sys.argv[2],
-    db = sys.argv[3],
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    databases = sys.argv[3]
     state_name_searched = sys.argv[4]
 
     db = MySQLdb.connect(
         host='localhost',
-        user='user',
-        passwd='passwd',
-        db=db,
+        user=username,
+        passwd=password,
+        db=databases,
         port=3306
     )
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE NAME LIKE '{}' \
-            ORDER BY states.id;".format(state_name_searched))
+        "SELECT * FROM states WHERE NAME LIKE '{}' ORDER BY states.id;".format(state_name_searched))
     states = cur.fetchall()
 
     for state in states:
