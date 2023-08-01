9b6bba51725c5d0345e89b03536d5b92f8857a03
BraianPerez97 <3603@holbertonstudents.com>
2023-08-01 12:58:54 -0400

    injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,18 +5,18 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
+    state_names = sys.argv[4]
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
-                         state_name_search=sys.argv[4],
                          port=3306)
 
     cur = db.cursor()
 
     cur.execute("SELECT * FROM state WHERE \
                 NAME = %s ORDER BY states.id;", (sys.argv[4],))
-    states = cur.fetchall()
+    rows = cur.fetchall()
 
-    for state in states:
-        print(state)
+    for row in rows:
+        print(row)
