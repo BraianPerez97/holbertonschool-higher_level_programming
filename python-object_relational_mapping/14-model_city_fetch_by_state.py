4547c6fa4dd78d8c373a4687d7ae643c89d8e111
BraianPerez97 <3603@holbertonstudents.com>
2023-08-04 19:51:17 -0400

    adding task ORM

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""List states"""
-
+"""
+Deletes all State objects with a name containing the letter a
+"""
 import sys
 from model_state import Base, State
-from model_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(sys.argv[1], sys.argv[2],
-                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(City, State).\
-        filter(City.state_id == State.id).all()
-    for city, state in result:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        session.delete(state)
+
     session.commit()
-    session.close()
