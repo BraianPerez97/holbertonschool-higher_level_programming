bf7c610df8a47bae216b5d081125bf7bdccf9ccc
BraianPerez97 <3603@holbertonstudents.com>
2023-08-04 19:35:19 -0400

    adding task ORM

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""List cities by states"""
-
+"""
+List all cities from a database
+"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, \
-                state.name FROM cities "
-                "JOIN states ON cities.state_id = states.state_id"
-                "ORDER BY cities.id")
-    rows = cur.fetchall()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    for row in rows:
-        print(row)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
